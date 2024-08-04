c268db21e33fc5b5edd4c9c2ca17e0fbf3a3a5a2
7yricaL <66447798+7yricaL@users.noreply.github.com>
2024-08-04 04:46:23 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -167,4 +167,5 @@ def delete_volunteer():
     return redirect(url_for('admin'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
